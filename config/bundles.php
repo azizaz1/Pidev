0cd4ae3fe88f65a5660db3978b06a7cad5953d2e
elkhater-elkharouf <76228304+elkhater-elkharouf@users.noreply.github.com>
2022-03-08 03:02:51 +0100

    add
    add

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
